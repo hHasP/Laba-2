6845dc909586e6d74ab1e124d4722ef846e450ae
Илья <71662397+hHasP@users.noreply.github.com>
2020-09-21 22:46:41 +0300

    Update index.C

--- index.C
+++ index.C
@@ -1,5 +1,5 @@
 #include <ctime>
-#include <iostream>
+#include <iostream> 
 #include <stdlib.h>
 #include <stdio.h>
 #define M 10
@@ -7,12 +7,12 @@
 int main() 
 {
     int a[N][M], i, j, max, min;
-    srand(time(NULL));
+    srand(time(NULL)); // параметр текущего системного времени, которое при каждом запускe программы будет разным // // Это позволит функции rand() каждый раз генерировать именно случайные числа//
     max = -1;
     for (j=0; j<M; j++) {
         min = 200;
         for (i=0; i<N; i++) {
-            a[i][j] = rand() % 200;
+            a[i][j] = rand() % 200; // генерирует число и далее вычисляется остаток от деления нa 200 от этого числа //
             if (a[i][j] < min) min = a[i][j];
         }
         if (min > max) max = min;
